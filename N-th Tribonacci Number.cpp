43c186064f2707e19cff83910d93c260588b7079
ManjushaJ <82754663+Mjp777@users.noreply.github.com>
2024-04-24 22:04:18 +0530

    Create  N-th Tribonacci Number.cpp

---
+++ N-th Tribonacci Number.cpp
@@ -0,0 +1,25 @@
+1137. N-th Tribonacci Number
+Easy
+Topics
+Companies
+Hint
+
+
+class Solution {
+ public:
+  int tribonacci(int n) {
+    if (n < 2)
+      return n;
+
+    vector<int> dp{0, 1, 1};
+
+    for (int i = 3; i <= n; ++i) {
+      const int next = dp[0] + dp[1] + dp[2];
+      dp[0] = dp[1];
+      dp[1] = dp[2];
+      dp[2] = next;
+    }
+
+    return dp[2];
+  }
+};
