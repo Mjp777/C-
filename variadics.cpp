d452422fd88452faca9be8d23b0ed870860e24b1
ManjushaJ <82754663+Mjp777@users.noreply.github.com>
2024-04-29 19:54:00 +0530

    Create variadics.cpp

---
+++ variadics.cpp
@@ -0,0 +1,84 @@
+variadics.cpp
+
+  
+ 4
+ 5
+ 6
+ 7
+ 8
+ 9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+//C++ Variadics in C++ - Hacker Rank Solution
+#include <iostream>
+using namespace std;
+
+// Enter your code for reversed_binary_value<bool...>()
+/* C++ Variadics in C++ - Hacker Rank Solution START */
+template <bool a> int reversed_binary_value() { return a; }
+
+template <bool a, bool b, bool... d> int reversed_binary_value() {
+  return (reversed_binary_value<b, d...>() << 1) + a;
+}
+/* C++ Variadics in C++ - Hacker Rank Solution END */
+
+template <int n, bool...digits>
+struct CheckValues {
+  	static void check(int x, int y)
+  	{
+    	CheckValues<n-1, 0, digits...>::check(x, y);
+    	CheckValues<n-1, 1, digits...>::check(x, y);
+  	}
+};
+
+template <bool...digits>
+struct CheckValues<0, digits...> {
+  	static void check(int x, int y)
+  	{
+    	int z = reversed_binary_value<digits...>();
+    	std::cout << (z+64*y==x);
+  	}
+};
+
+int main()
+{
+  	int t; std::cin >> t;
+
+  	for (int i=0; i!=t; ++i) {
+		int x, y;
+    	cin >> x >> y;
+    	CheckValues<6>::check(x, y);
+    	cout << "\n";
+  	}
+}
