a88ea2f9a15674c936ecb831fd03dc1ff5000872
ManjushaJ <82754663+Mjp777@users.noreply.github.com>
2024-05-04 22:32:04 +0530

    Create Boats to Save People.cpp

---
+++ Boats to Save People.cpp
@@ -0,0 +1,18 @@
+Boats to Save People
+
+class Solution {
+ public:
+  int numRescueBoats(vector<int>& people, int limit) {
+    int ans = 0;
+
+    ranges::sort(people);
+
+    for (int i = 0, j = people.size() - 1; i <= j; ++ans) {
+      const int remain = limit - people[j--];
+      if (people[i] <= remain)
+        ++i;
+    }
+
+    return ans;
+  }
+};
